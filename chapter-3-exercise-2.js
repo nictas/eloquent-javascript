e026d9842a68583b5751829bc74566ad858dd562
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2023-12-19 15:56:06 +0200

    Add exercise 2 of chapter 3

---
+++ chapter-3-exercise-2.js
@@ -0,0 +1,43 @@
+const isEven = number => {
+    const isEvenRecursive = number => {
+        if (number === 0) {
+            return true;
+        }
+        if (number === 1) {
+            return false;
+        }
+        return isEvenRecursive(number - 2);
+    }
+    if (!isFiniteNumber(number) || !isInt(number)) {
+        return false;
+    }
+    return isEvenRecursive(Math.abs(number));
+}
+
+const isFiniteNumber = number => typeof number == 'number' && !Number.isNaN(number) && Number.isFinite(number);
+
+const isInt = number => {
+    return number % 1 === 0;
+}
+
+module.exports = isEven;
+
+if (require.main === module) {
+    console.log(`isEven(2): ${isEven(2)}`);
+    console.log(`isEven(3): ${isEven(3)}`);
+    console.log(`isEven(11112): ${isEven(11112)}`);
+    console.log(`isEven(11113): ${isEven(11113)}`);
+    console.log(`isEven(-2): ${isEven(-2)}`);
+    console.log(`isEven(-3): ${isEven(-3)}`);
+    console.log(`isEven(-11112): ${isEven(-11112)}`);
+    console.log(`isEven(-11113): ${isEven(-11113)}`);
+    console.log(`isEven(3.14): ${isEven(3.14)}`);
+    console.log(`isEven(0.01): ${isEven(0.01)}`);
+    console.log(`isEven(+Infinity): ${isEven(+Infinity)}`);
+    console.log(`isEven(-Infinity): ${isEven(-Infinity)}`);
+    console.log(`isEven(NaN): ${isEven(NaN)}`);
+    console.log(`isEven("a"): ${isEven("a")}`);
+    console.log(`isEven(true): ${isEven(true)}`);
+    console.log(`isEven(null): ${isEven(null)}`);
+    console.log(`isEven(undefined): ${isEven(undefined)}`);
+}
---
+++ chapter-3-exercise-2.test.js
@@ -0,0 +1,30 @@
+const isEven = require('./chapter-3-exercise-2');
+
+test('isEven returns true for positive even numbers and false for odd ones', () => {
+  expect(isEven(2)).toBe(true);
+  expect(isEven(3)).toBe(false);
+  expect(isEven(11112)).toBe(true);
+  expect(isEven(11113)).toBe(false);
+});
+
+test('isEven returns true for negative even numbers and false for odd ones', () => {
+  expect(isEven(-2)).toBe(true);
+  expect(isEven(-3)).toBe(false);
+  expect(isEven(-11112)).toBe(true);
+  expect(isEven(-11113)).toBe(false);
+});
+
+test('isEven returns false for fractions', () => {
+  expect(isEven(3.14)).toBe(false);
+  expect(isEven(0.01)).toBe(false);
+});
+
+test('isEven return false for values other than numbers', () => {
+  expect(isEven(+Infinity)).toBe(false);
+  expect(isEven(-Infinity)).toBe(false);
+  expect(isEven(NaN)).toBe(false);
+  expect(isEven("a")).toBe(false);
+  expect(isEven(true)).toBe(false);
+  expect(isEven(null)).toBe(false);
+  expect(isEven(undefined)).toBe(false);
+});
