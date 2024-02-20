910da41c87a56dc07a526a1fd716ad032c9e5a02
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-02-20 09:20:36 +0200

    Enhance example 5 of chapter 6

--- chapter-6/example-5.test.js
+++ chapter-6/example-5.test.js
@@ -16,3 +16,17 @@ test('Map.set works as expected', () => {
     expect(ages.get('Thrall')).toBe(32);
     expect(ages.get('Arthas')).toBe(22);
 });
+
+test('Objects extend Object.prototype and inherit their properties', () => {
+    let test = {
+        a: 1,
+        b: 2,
+        c: 3
+    };
+    expect('toString' in test).toBe(true);
+    expect('a' in test).toBe(true);
+    expect('b' in test).toBe(true);
+    expect(test.hasOwnProperty('toString')).toBe(false);
+    expect(test.hasOwnProperty('a')).toBe(true);
+    expect(test.hasOwnProperty('b')).toBe(true);
+});
